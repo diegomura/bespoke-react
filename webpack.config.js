a4926b6e372002c5191e680c18ffb2b7c3dc893e
Diego Muracciole <diegomuracciole@gmail.com>
2017-10-22 22:14:43 -0300

    Webpack initial setup

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, './src'),
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+}
